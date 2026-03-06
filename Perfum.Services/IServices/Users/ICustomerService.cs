6eb8e6cf32b45e8315caf29f1f9b8f326bc1512e
muhmedsaeed <mohamed.saied.5755@gmail.com>
2026-03-07 01:17:35 +0200

    my version before pull

--- Perfum.Services/IServices/Users/ICustomerService.cs
+++ Perfum.Services/IServices/Users/ICustomerService.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿
 namespace Perfum.Services.IServices.Users;
 
@@ -8,7 +7,8 @@ public interface ICustomerService
     Task<string> AddAsync(AddCustomerVM model);
 
     // --------------------- Read ---------------------
-    Task<PagedResult<CustomerVM, ViewModels.UserVM.CustomersFilter, DashBoardCustomer>> GetAllAsync(CustomersFilter? filter);
+    Task<PagedResult<CustomerVM, CustomersFilter, DashBoardCustomer>> GetAllAsync(CustomersFilter? filter);
+
     Task<CustomerVM> GetByIdAsync(int id);
 
     Task<CustomerVM> GetByEmailAsync(string email);
@@ -18,12 +18,4 @@ public interface ICustomerService
 
     // --------------------- Delete ---------------------
     Task<string> DeleteAsync(int id);
-=======
-﻿namespace Perfum.Services.IServices.Users;
-
-public interface ICustomerService
-{
-
-    Task<Customer> GetAllAsync(Customer customer);
->>>>>>> 8dee4014fe6596003dadc632b51af84721b68726
 }
